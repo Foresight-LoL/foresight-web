f9e31ecf314e45c473da9b6c245c29a08e586da4
ldracea <dracea.lucian.mihai@gmail.com>
2026-06-14 22:16:40 +0300

    - added react frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <main className="page">
+            <h1>Welcome to Foresight</h1>
+            <p>Coming soon.</p>
+            <img src="/claudiu_funny.png" alt="Claudiu funny"/>
+        </main>
+    )
+}
+
+export default App
